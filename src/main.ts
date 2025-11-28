e964d7b62b604670f5b83e940a037ce813befb7c
samuelIkoli <ayibanimiikoli@gmail.com>
2025-11-28 14:57:40 +0100

    added swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { VersioningType, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('AFF API')
+    .setDescription('AFF backend API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('v1/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
