0995767876d61589650c5ccb851da68b9fb9bcef
samuelIkoli <ayibanimiikoli@gmail.com>
2026-06-03 17:23:49 +0100

    ft: modify assigned_tailor_id column to include default NULL in projects table

--- prisma/migrations/20260602100000_add_assigned_tailor/migration.sql
+++ prisma/migrations/20260602100000_add_assigned_tailor/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "projects" ADD COLUMN "assigned_tailor_id" TEXT;
+ALTER TABLE "projects" ADD "assigned_tailor_id" TEXT DEFAULT NULL;
 
 -- AddForeignKey
 ALTER TABLE "projects" ADD CONSTRAINT "projects_assigned_tailor_id_fkey" FOREIGN KEY ("assigned_tailor_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
